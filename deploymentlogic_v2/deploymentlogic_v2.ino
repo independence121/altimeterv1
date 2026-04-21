1ac1935c585e339c01da4cf21615bfc18aed09a9
independence121 <iaa1011@utulsa.edu>
2026-04-20 23:13:16 -0500

    code cleanup

--- deploymentlogic_v2/deploymentlogic_v2.ino
+++ deploymentlogic_v2/deploymentlogic_v2.ino
@@ -3,100 +3,131 @@
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
 #include <LittleFS.h>
+#include <RFM69.h>
+#include <SPI.h>
+#include <stdio.h>
 
-
-Adafruit_BMP280 bmp;
-Adafruit_ADXL375 accel = Adafruit_ADXL375(12345);
+// Addressing and settings
+#define NODEID 1       // The ID of this node
+#define NETWORKID 100  // The network ID (must be same for all nodes)
+#define GATEWAYID 2    // The ID of the receiving node
+#define FREQUENCY RF69_433MHZ
+#define FREQUENCY_EXACT 433000000      // Match your hardware (433/868/915)
+#define ENCRYPTKEY "sampleEncryptKey"  // 16 characters exactly
+#define CS_PIN D6
+#define IRQ_PIN D9
+#define RST D4
 
 #define DROGUE A0
 #define MAIN A1
 #define G 9.81
 #define SEA_LEVEL_HPA 1008.9
 
-int launchTime, lastTime, drogueTime;
+int launchTime, lastTime;
+int accelCounter, maxCounter;
 int state;
 int descending;
+float potentialMaxHeight;
 float initialHeight, currentHeight, maxHeight;
+float height1, height2, height3, height4, height5;
 float realAltitude;
 float x_offset, y_offset, z_offset;
 float a_mag;  //acceleration magnitude
 
-bool launch = false;
-bool drogueDeployed = false;
+
+char maxAltitudeStr[15];
+char initialHeightStr[15];
+
+Adafruit_BMP280 bmp;
+Adafruit_ADXL375 accel = Adafruit_ADXL375(12345);
+RFM69 radio(CS_PIN, IRQ_PIN, true);
 
 void setup() {
+  pinMode(DROGUE, OUTPUT);
+  pinMode(MAIN, OUTPUT);
   digitalWrite(DROGUE, LOW);
   digitalWrite(MAIN, LOW);
 
-  pinMode(DROGUE, OUTPUT);
-  pinMode(MAIN, OUTPUT);
+  pinMode(RST, OUTPUT);
+  digitalWrite(RST, HIGH);
   delay(1000);
-  Serial.begin(115200);
-  //serial test
-  while (!Serial) delay(100);  // wait for native usb
-  Serial.println(F("BMP280 test"));
+  digitalWrite(RST, LOW);
+  radio.initialize(FREQUENCY, NODEID, NETWORKID);
+  radio.setHighPower();  // Necessary for HCW models
+  radio.encrypt(ENCRYPTKEY);
+  radio.setFrequency(FREQUENCY_EXACT);
+
+  transmitText("BMP280 test");
   unsigned status;
   //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
   status = bmp.begin();
   if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                     "try a different address!"));
-    Serial.print("SensorID was: 0x");
-    Serial.println(bmp.sensorID(), 16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    transmitText(F("Could not find a valid BMP280 sensor, check wiring or "
+                   "try a different address!"));
     while (1) delay(10);
   }
-   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X8,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X4,      /* Filtering. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,   /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,   /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X8,   /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X4,     /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
-  Serial.print("Hello!");
   if (!accel.begin()) {
     /* There was a problem detecting the ADXL375 ... check your connections */
-    Serial.println("No ADXL375 detected");
+    transmitText("No ADXL375 detected");
     while (1)
       ;
   }
+  accel.setDataRate(ADXL3XX_DATARATE_200_HZ);
   initialHeight = bmp.readAltitude(SEA_LEVEL_HPA);
   lastTime = 0;
+  accelCounter = 0;
+  maxCounter = 0;
+  potentialMaxHeight = 0;
   calibrate();
 
   if (!LittleFS.begin(true)) {
-    Serial.print("mount failed");
+    transmitText("mount failed");
   }
+  transmitText("Ready for flight! Initial Height:");
+  floatToString(initialHeight, initialHeightStr);
+  transmitText(initialHeightStr);
 }
 
 void loop() {
   switch (state) {
     case 0:  //pad
       //grabbing accel data until launch detected
-      if (getAccelDebug() > 4 * G) {  //detected launch
+      if (getAccel() > 10 * G) {
+        accelCounter++;  //detected launch
+      } else {
+        accelCounter = 0;
+      }
+      if (accelCounter > 20) {
         state = 1;
         launchTime = millis();
+        transmitText("Liftoff!");
       }
       break;
     case 1:  //motor burning
-      if (getAccelDebug() < 2 * G && millis() > (launchTime + 7000)) {
+      if (getAccel() < 2 * G && millis() > (launchTime + 7000)) {
         state = 2;  //detected coast - motor burned out
+        transmitText("Now coasting");
       }
       break;
     case 2:  // coasting
-      if (millis() > lastTime + 50) {
+      if (millis() > lastTime + 100) {
         lastTime = millis();
         if (checkDescent()) {
           deployDrogue();
           state = 3;
         }
       }
+      if (millis() > launchTime + 60000) {
+        deployDrogue();
+        state = 3;
+      }
       break;
     case 3:  //drogue
-      if (millis() > drogueTime + 2000) {
-        digitalWrite(DROGUE, LOW);
-      }
       realAltitude = bmp.readAltitude(SEA_LEVEL_HPA) - initialHeight;
       if (realAltitude < 244) {
         deployMain();
@@ -108,7 +139,7 @@ void loop() {
       break;
   }
 }
-float getAccelDebug() {
+float getAccel() {
   sensors_event_t event;
   accel.getEvent(&event);
 
@@ -125,31 +156,52 @@ bool checkDescent() {
   if (currentHeight > maxHeight) {
     maxHeight = currentHeight;
   }
-  if (currentHeight < maxHeight - 2) {
-    descending++;
-  } else {
-    descending = 0;
-  }
-  if (descending > 10) {
+  height5 = height4;
+  height4 = height3;
+  height3 = height2;
+  height2 = height1;
+  height1 = currentHeight;
+  if (height5 > height4 && height4 > height3 && height3 > height2 && height2 > height1) {
     return true;
   } else {
     return false;
   }
+
+
+  // if (currentHeight > maxHeight) {
+  //   if (currentHeight > potentialMaxHeight) {
+  //     potentialMaxHeight = currentHeight;
+  //     maxCounter++;
+  //   } else {
+  //     maxCounter = 0;
+  //   }
+  //   if (maxCounter > 3) {
+  //     maxHeight = potentialMaxHeight;
+  //   }
+  // }
+  // if (currentHeight < maxHeight - 5) {
+  //   descending++;
+  // } else {
+  //   descending = 0;
+  // }
+  // return (descending > 10);
 }
+
 
 void saveAltitude() {
   maxHeight -= initialHeight;
+  floatToString(maxHeight, maxAltitudeStr);
+  transmitText("Max altitude: ");
+  transmitText(maxAltitudeStr);
   File f = LittleFS.open("/flightlog.txt", "w");
   if (f) {
     f.print("Max Altitude: ");
     f.println(maxHeight);
     f.close();
-    Serial.println("write OK");
+    transmitText("write OK");
   } else {
-    Serial.println("write FAILED");
+    transmitText("write FAILED");
   }
-  Serial.print("Max altitude: ");
-  Serial.println(maxHeight);
   return;
 }
 
@@ -171,11 +223,28 @@ void calibrate() {
 
 void deployMain() {
   digitalWrite(MAIN, HIGH);
+  transmitText("main deployed");
   delay(2000);
   digitalWrite(MAIN, LOW);
 }
 
 void deployDrogue() {
   digitalWrite(DROGUE, HIGH);
-  drogueTime = millis();
+  delay(2000);
+  digitalWrite(DROGUE, LOW);
+  transmitText("drogue deployed");
+}
+
+void floatToString(float message, char* buf) {
+  dtostrf(message, 15, 3, buf);
+}
+
+void transmitText(String message) {
+
+  // Convert String to a character buffer
+  char buffer[message.length() + 1];
+  message.toCharArray(buffer, message.length() + 1);
+
+  // sendWithRetry(ReceiverID, Buffer, BufferLength)
+  radio.send(GATEWAYID, buffer, strlen(buffer));
 }
